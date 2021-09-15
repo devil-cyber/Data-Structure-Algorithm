b3e09a6e97c553f968fd98dd5988264d1e67ef82
devil-cyber <mani2474695@gmail.com>
2021-09-16 02:11:55 +0530

    Corrected the code

--- heap/median_stream.py
+++ heap/median_stream.py
@@ -1,35 +1,34 @@
-## There is some eror in problem
+
 import heapq as h
 
 
-def MedianStream(arr):
-    s = []
-    g = []
+def median(arr):
+    s = [] # max heap
+    g = [] # min heap
     n = len(arr)
-    h.heappush(s,arr[0])
-    print(s[0])
-    for i in range(1, n):
+    h.heappush(s, -arr[0])
+    print(arr[0])
+    for i in range(1,n):
         x = arr[i]
         if len(s) > len(g):
-            if max(s)>x:
-                sdata = s.pop()
-                h.heapify(s)
-                h.heappush(g,sdata)
-                h.heappush(s,x)
+            if (-s[0]) > x:
+                sdata = -h.heappop(s)
+                h.heappush(g, sdata)
+                h.heappush(s,-x)
             else:
                 h.heappush(g,x)
-                print((max(s)+max(g))/2)
+            print(((-s[0])+g[0])/2)
         else:
-            if x <= max(s):
-                h.heappush(s,x)
+            if x <= (-s[0]):
+                h.heappush(s, -x)
             else:
-                h.heappush(g,x)
+                h.heappush(g, x)
                 gdata = h.heappop(g)
-                h.heappush(s,gdata)
-                print(max(s))
+                h.heappush(s, -gdata)
+            print(-s[0])
 
 
 
 if __name__ == "__main__":
     arr = [12,15,10,5,8,7,16]
-    MedianStream(arr)
+    median(arr)
